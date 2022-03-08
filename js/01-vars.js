274727d11513393e0aed9236fb73cd48e0123554
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-08 21:21:42 +0200

    save

---
+++ js/01-vars.js
@@ -0,0 +1,34 @@
+// let x;
+// console.log('10');
+
+// *__________________________________________________________________
+// *Ввод пользователя
+// *
+// *методы window.confirm() и window.prompt();
+// ________________________________________________________________
+// const shouldRenew = confirm('хотите продлить подписку?');
+// console.log(shouldRenew);
+
+// let quantity = prompt('Введите количество товара?');
+// quantity = Number(quantity);
+// console.log(quantity);
+// console.log(typeof quantity);
+
+// *__________________________________________________________________
+// *Парс числа с Number.parseInt() и Number.parseFloat()
+// __________________________________________________________________
+// let elementWidth = '50px';
+// const result = Number.parseInt(elementWidth);
+// console.log(result);
+
+// let elementHeight = '200.75px';
+// const result = Number.parseFloat(elementHeight);
+// console.log(result);
+
+// *__________________________________________________________________
+// * Метод число.toFixed(digits)
+// *__________________________________________________________________
+// let salary = 1300.16578;
+// salary = Number(salary.toFixed(2));
+// salary = Number(salary); для уменьшения кода можно писать только верхнюю строку
+// console.log(salary);
