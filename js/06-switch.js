9fc7454dd66ffce352ff2513c04ac40ecacb107f
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-11 21:20:07 +0200

    save

---
+++ js/06-switch.js
@@ -0,0 +1,24 @@
+// * Напиши срипт выбора стоимости отеля по кол-ву звезд:
+// 1 - 20$, 2 - 30$, 3 - 50$, 4 - 70$, 5 - 120$
+// const stars = 10;
+// let price;
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 30;
+//         break;
+//     case 3:
+//         price = 50;
+//         break; 
+//     case 4:
+//         price = 70;
+//         break;
+//     case 5:
+//         price = 120;
+//         break;
+//     default:
+//         console.log(`Такого кол-ва звезд нет`);           
+// }
+// console.log(price);
