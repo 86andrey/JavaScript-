d43b5ef43d7fd6944108411b61561525a85fb9d1
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-17 13:19:30 +0200

    save

--- js/08-array.js
+++ js/08-array.js
@@ -55,7 +55,57 @@
 // console.log(message);
 
 // ___________________________________________________
-// * Напиши скрипт поиска логина
-// const numbers = [1,2,3,4,5,6];
-// numbers.unshift(6);
-// console.table(numbers);
+// * Напиши скрипт поиска самого мальнького числа в массиве
+// * при условии что числа уникальные (не повторяются)
+// условие - если к первому числу ты меньше - то замени его,
+//  иначе продолжай дальше
+// const numbers = [51, 18, 13, 24, 7, 2, 19];
+// let min = numbers[0];
+// for (const number of numbers) {
+//     if (number < min) {
+//         min = number;
+//     } 
+// }
+// console.log(min);
+
+// ___________________________________________________
+// * Напиши скрипт который обьединяет все элементы массива
+//  в одно-строковое значение.
+// - элементов может быть произвольное кол-во.
+// - пусть элементы массива в строку будут разделены запятой.
+// - сначало через for.
+// - потом через join().
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// let newfriends = '';
+// for (const frend of friends) {
+//     newfriends += frend +',';
+// }
+// newfriends = newfriends.slice(0, -1);
+// console.log(newfriends);
+// - должно получиться 'Mango,Poly,Kiwi,Ajax' ;
+
+// ___________________________________________________
+// * Напиши скрипт который заменяет регистр каждого символа
+// в строке на противоположный;
+// например: если строка "JavaScript", то на выходе должен быть
+// строка "jAVAsCRIPT".
+// ** 1-ый вариант - олдскул
+// const string = 'JavaScript';
+// let uplit = string.split('');
+// let inver = '';
+// for (const lit of string) {
+//     if (lit === lit.toLowerCase()) {
+//         inver += lit.toUpperCase();
+//     } else {inver += lit.toLowerCase();
+//     }
+// }
+// console.log(inver);
+
+// ** 2-ый вариант - хорош
+// const string = 'JavaScript';
+// let uplit = string.split('');
+// let inver = '';
+// for (const lit of string) {
+//     inver += lit === lit.toLowerCase() ? lit.toUpperCase() : lit.toLowerCase();
+// }
+// console.log(inver);
