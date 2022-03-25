9e297c4ddb8253f0edc1d4abb78c9223ed408257
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-25 11:24:01 +0200

    save

--- js/09-function.js
+++ js/09-function.js
@@ -64,3 +64,17 @@
 //   return num;
 //  }
 //  console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+
+for (const key in book) {
+  // Ключ
+  // console.log(key);
+  // Значение свойства с таким ключом
+  console.log(book[key]);
+ }
