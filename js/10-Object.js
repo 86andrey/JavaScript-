5a7ef11042c478ff08afb83fef502fabc68e8a5c
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-27 20:37:37 +0300

    save

--- js/10-Object.js
+++ js/10-Object.js
@@ -1,10 +1,20 @@
-const playlist = {
-    ret: 'мой супер плейлист',
-    rating: 2,
-    tracks: ['Track-1','Track-2','Track-3'],
-    trackCount: 4,
-};
+// const playlist = {
+//     ret: 'мой супер плейлист',
+//     rating: 2,
+//     tracks: ['Track-1','Track-2','Track-3'],
+//     trackCount: 4,
+//     getName: function () {
+// console.log('you are president?');
+//     },
+//     addTrack(track) {
+//         this.tracks.push(track);
+//     },
+
+// };
+// playlist.getName();
+// playlist.addTrack('new-classic');
 // console.log(playlist);
+
 // const x = {};
 // console.log({});
 // const fn = function (myObject) {
@@ -15,5 +25,39 @@ const playlist = {
 // const sd = function () {
 //     return {a: 5};
 // }
-const supe = 'ret';
-console.log(playlist[supe]);
+
+// const friends = [
+//     {name: 'Mango', online: false},
+//     {name: 'kiwi', online: false},
+//     {name: 'poly', online: true},
+//     {name: 'ajax', online: false},
+// ];
+// console.log(Object.keys(friends).length);
+// for (const friend of friends) {
+//     console.log(friend.name);   
+// }
+
+// const getOnlineFriends = function (allFriends) {
+//     const friendOnline = [];
+// for (const friend of allFriends) {
+//     if (friend.online) {
+//         friendOnline.push(friend);
+//     }  
+// } return friendOnline;
+// }
+// console.log(getOnlineFriends(friends));
+
+const cart = {
+    items: [],
+    getItems() {},
+    add(product) {},
+    remove(productName) {},
+    clear() {},
+    countTotalPrice () {},
+    increaseQuantity(productName) {},
+    decreaseQuantity(productName) {},
+};
+card.add({name: 'яблоко', price: 50});
+card.add({name: 'яблоко', price: 50});
+card.add({name: 'яблоко', price: 50});
+card.add({name: 'яблоко', price: 50});
