78706f740db65a8c6e20650d267ab01ebdb56bf7
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-27 12:17:13 +0300

    save

---
+++ js/10-Object.js
@@ -0,0 +1,19 @@
+const playlist = {
+    ret: 'мой супер плейлист',
+    rating: 2,
+    tracks: ['Track-1','Track-2','Track-3'],
+    trackCount: 4,
+};
+// console.log(playlist);
+// const x = {};
+// console.log({});
+// const fn = function (myObject) {
+//     console.log(myObject);
+// }
+// fn({a: 4, b: 8});
+
+// const sd = function () {
+//     return {a: 5};
+// }
+const supe = 'ret';
+console.log(playlist[supe]);
