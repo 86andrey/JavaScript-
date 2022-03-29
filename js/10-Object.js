9bacd853c52383ab715c79428097cfa5c3902117
Andrey Ivanov <prioritet2503@gmail.com>
2022-03-29 11:04:32 +0300

    save

--- js/10-Object.js
+++ js/10-Object.js
@@ -47,17 +47,69 @@
 // }
 // console.log(getOnlineFriends(friends));
 
-const cart = {
-    items: [],
-    getItems() {},
-    add(product) {},
-    remove(productName) {},
-    clear() {},
-    countTotalPrice () {},
-    increaseQuantity(productName) {},
-    decreaseQuantity(productName) {},
-};
-card.add({name: 'яблоко', price: 50});
-card.add({name: 'яблоко', price: 50});
-card.add({name: 'яблоко', price: 50});
-card.add({name: 'яблоко', price: 50});
+// const cart = {
+//     items: [],
+//     getItems() {},
+//     add(product) {},
+//     remove(productName) {},
+//     clear() {},
+//     countTotalPrice () {},
+//     increaseQuantity(productName) {},
+//     decreaseQuantity(productName) {},
+// };
+// card.add({name: 'яблоко', price: 50});
+// card.add({name: 'яблоко', price: 50});
+// card.add({name: 'яблоко', price: 50});
+// card.add({name: 'яблоко', price: 50});
+
+// function countProps(object) {
+//     // Change code below this line
+//     let propCount = 0;
+//   const keys = Object.keys(object);
+  
+//     for (const key of keys) {
+//       if (object.hasOwnProperty(key)) {
+//         propCount = Object.values(object).length;
+//       };
+//     };
+  
+//     return propCount;
+//     // Change code above this line
+//   };
+//   console.log(countProps(object));
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getProductPrice(tron) {
+  const all = [];
+  for (const key of products) {
+    if (1 === 1){
+      all.push(key[tron]);
+    }
+  }; return all;
+}
+console.log(getProductPrice("name"));
+// console.log(products);
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+// let allProp = [];
+// function getAllPropValues(propName) {
+// for (const key of products) {
+//   // console.log(key);
+//   if (key.name === propName) {
+//   return products[key]};
+  
+// }; return 2;
+// }
+// console.log(getAllPropValues("Radar"));
