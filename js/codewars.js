fc1ba4776daf74c992de54aa39d8985b93016a5f
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-08 14:15:02 +0300

    save

--- js/codewars.js
+++ js/codewars.js
@@ -223,3 +223,12 @@
 //   console.log(`Login: ${login.value}, Password: ${password.value}`);
 //   event.currentTarget.reset();
 // }
+const button = document.querySelector(".btn");
+
+const handleClick = (event) => {
+  console.log("event: ", event);
+  console.log("event type: ", event.type);
+  console.log("currentTarget: ", event.currentTarget);
+};
+
+button.addEventListener("click", handleClick);
